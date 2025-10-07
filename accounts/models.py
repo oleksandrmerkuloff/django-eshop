6b6d7abbf5e5f882cb762d9f534b87393293ccb9
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-10-07 13:54:09 +0300

    added custom user model, config it in settings.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, phone=None, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+            self,
+            email,
+            first_name,
+            last_name,
+            phone,
+            password=None
+            ):
+        user = self.create_user(email, first_name, last_name, phone, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        unique=True
+    )
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(
+        max_length=50,
+    )
+    phone = models.CharField(
+        max_length=25,
+        blank=True,
+        null=True
+    )
+    registrated_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'last_name'
+    ]
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    @property
+    def is_staff(self):
+        return self.is_admin
